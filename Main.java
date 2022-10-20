4f899280a4d4503c7ce9c1fe9479a7d8ffe7c15c
qaAutomations <107888756+qaAutomations@users.noreply.github.com>
2022-10-20 14:36:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //hiii this is hodi
+        /* this is a comment section
+        * we can write comments in those rows
+        *  those rows
+        *
+        */
+        /*System.out.println("Moris");
+        System.out.print(17+5);
+        System.out.println('$');
+        System.out.println(true);
+        System.out.println(false);*/
+        /*byte age = 12 ;
+        short temp = -22;
+        int salary = 330_000;
+        long gmail_users = 5_000_000_000_000_000_000l;
+        float rate1= 3.1495155f;
+        double rate2= 3.1495155555456466;
+        boolean isPos = true;
+        char operator = '+';
+        String name = "Bat-El";
+        */
+        //String car ;
+        //car = "audi" ;
+        //String car1 ="audi";
+//
+        //int st1= 100;
+        //int st2= 90;
+        //int st3= 10;
+        //int st4= 50;
+//
+        //int st5=100,st6=90,st7=10,st8=44;
+//
+        //System.out.println(car);
+//        int x= 10;
+//        int y = 15;
+//
+//        System.out.println(x-y);
+//        System.out.println(x+y);
+//        System.out.println(x*y);
+//        System.out.println(x/y);
+//        System.out.println(x%y);
+        //STRING
+//        String name = "hothaifa ZOUBI";
+//        String firstName= " hothaifa";
+//
+//        System.out.println( name.toUpperCase() ); // change the lower to upper case
+//        System.out.println( name.toLowerCase() ); // change the upper to lower case
+//        System.out.println( name.indexOf("afia") ); // returns the index of the string
+//        System.out.println( name.contains(" zou") ); // check if string contains another text
+//        System.out.println(name.contains(firstName));
+//        System.out.println(name.length()); // return the string length
+//        System.out.println(name.endsWith(".com")); // check if the string ends with
+//        System.out.println(name.replace(' ','.')); //replace char with another
+//
+//        String q= " \"with great power comes great \" ";
+//        q= " \\with \t great \n power  \b comes great \\ ";
+//        System.out.println(q);
+
+        //
+        /*
+        String qoute = "DONT STOP UNTIL YOU PROUD";
+        //1
+        System.out.println( qoute.toLowerCase() );
+        //2
+        System.out.println( qoute.toUpperCase() );
+        //3
+        System.out.println( qoute.length() );
+        //4
+        String newQoute = "\""+qoute+"\"";
+        System.out.println(newQoute);
+        //5
+        System.out.println(newQoute.toLowerCase());
+*/
+
+        // Math
+//        int x = 8;
+//        int y = 5;
+//        int max = Math.max(x,y);
+//        int small = Math.min(x,y);
+//        System.out.println( "max:"+ max  );
+//        System.out.println( "small:"+small  );
+//        System.out.println(Math.pow(x,3));
+//        System.out.println(Math.sqrt(100));
+//        System.out.println(Math.abs(-55));
+//        System.out.println(Math.floor(5.65));
+//        System.out.println(Math.ceil(5.65));
+//        System.out.println(Math.round(5.65));
+//        System.out.println(Math.random());
+//
+//        int omriAge = 29;
+//        int benAge = 21;
+//
+//        System.out.println(Math.max(omriAge,benAge));
+//        System.out.println(Math.sqrt(benAge));
+//        System.out.println(   Math.ceil( Math.sqrt(  omriAge   )   )   );
+//
+        //Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter your name :");
+//        String userName = scanner.nextLine();
+//        System.out.println("Please enter your age :");
+//        int userAge= scanner.nextInt();
+//        System.out.println("hiiii   "+userName);
+//        System.out.println("your age is "+userAge);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter a number : ");
+        int number1= scanner.nextInt();
+        System.out.println("enter a number : ");
+        int number2= scanner.nextInt();
+
+        System.out.println("the sum of the 2 numbers is :" + (number1+number2) );
+
+    }
+
+}
+
